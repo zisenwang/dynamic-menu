7885ee02764ccbadc15267c8d3cb1558fbb0b118
zisenwang <xg3000wzzzz@gmail.com>
2023-09-18 14:05:58 +0800

    import bootstrap

--- dynamic-menu-angular/src/app/app.module.ts
+++ dynamic-menu-angular/src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    isDevMode() ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
